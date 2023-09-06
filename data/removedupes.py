6b06b2fad6e470532039ff162a40cda874b2d015
KristianAskS <kristians.sin.konto@gmail.com>
2023-09-06 13:25:34 +0200

    fixed some things

---
+++ data/removedupes.py
@@ -0,0 +1,12 @@
+
+
+# remove dupes from enable2k.txt#
+# and write in it 
+
+with open("./enable2k.txt", "rt") as f:
+    data = f.read().splitlines()
+
+data = list(set(data))
+
+with open("./enable2k.txt", "wt") as f:
+    f.write("\n".join([x.lower() for x in data]))
